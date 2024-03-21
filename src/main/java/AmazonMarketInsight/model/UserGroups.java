2fde66edb772199c0f86d956ccdb1e5030476e6a
Jiajie He <jiajiehe@yahoo.com>
2024-03-21 13:14:17 -0700

    uploading maanvi's files

---
+++ src/main/java/AmazonMarketInsight/model/UserGroups.java
@@ -0,0 +1,63 @@
+package AmazonMarketInsight.model;
+
+import java.util.Timestamp;
+
+public class UserGroups {
+    private int groupId;
+    private String groupName;
+    private String role;
+    private Timestamp joinDate;
+    private String userName;
+    private Category category;
+
+    // Constructors, getters, setters
+   
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Timestamp getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Timestamp joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
